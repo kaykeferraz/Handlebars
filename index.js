b50eceb5e9da1bd051525aa4a24bdb584f6eaa27
EA - Kayke Ferraz De Moraes <kayke_ferraz@educadventista.org>
2023-11-27 14:50:10 -0300

    estruturando um projeto com handlebars

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 app.engine('handlebars', exphbs .engine())
 app.set('view engine','handlebars')
 
+app.use(express.static(public))
+
 app.get('/', (requisiaao,resposta) =>{
     resposta.render("home")
 })
